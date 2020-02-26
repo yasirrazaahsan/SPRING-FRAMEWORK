2880948d3c18971c13f80abe85273420cb3d8f9f
Yasir_Raza <Yasir_Raza@Yasir_Raza-PC>
2020-02-27 00:51:17 +0530

    2nd COMMIT!!!

--- Spring-Security-awt-Example/src/main/java/com/javatechie/awt/api/SpringSecurityAwtExampleApplication.java
+++ Spring-Security-awt-Example/src/main/java/com/javatechie/awt/api/SpringSecurityAwtExampleApplication.java
@@ -32,6 +32,7 @@ public class SpringSecurityAwtExampleApplication extends SpringBootServletInitia
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityAwtExampleApplication.class, args);
+		System.out.println("................Now I just updated the file and Saved it........");
 	}
 
 	@Override
